622fcdccdd550e06f000f96c9bf2fc9b67c4b1d9
notneerajkumarsah <notneerajkumarsah@gmail.com>
2026-03-02 02:38:34 +0530

    created frontend ui dashboards

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,19 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './config/db.js';
+import path from 'path';
+import invoiceRouter from './routes/invoiceRouter.js';
+import businessProfileRouter from './routes/businessProfileRouter.js';
+import aiInvoiceRouter from './routes/aiInvoiceRouter.js';
 
 const app = express();
 const port =4000;
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+// app.use(cors());
 app.use(clerkMiddleware())
 app.use(express.json({limit: "20mb"}))
 app.use(express.urlencoded({limit:"20mb" , extended: true}));
@@ -16,6 +24,11 @@ app.use(express.urlencoded({limit:"20mb" , extended: true}));
 connectDB()
 
 // routes
+app.use('/uploads', express.static(path.join(process.cwd(),"uploads")));
+app.use('/api/invoice', invoiceRouter);
+console.log("BusinessProfile route loaded");//....remove
+app.use('/api/businessProfile',businessProfileRouter);
+app.use('/api/ai',aiInvoiceRouter);
 app.get('/', (req,res)=> {
     res.send("API WORKING")
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AppShell from "./components/AppShell";
+import Dashboard from "./pages/Dashboard";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/clerk-react";
+import CreateInvoice from "./pages/CreateInvoice";
+import Invoices from "./pages/Invoices";
 
+// const ClerkProtected = ({ children }) => (
+//   <>
+//     <SignedIn>{children}</SignedIn>
+//     <SignedOut>
+//       <RedirectToSignIn />
+//     </SignedOut>
+//   </>
+// );
+const ClerkProtected = ({ children }) => {
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
+
+  if (!isSignedIn) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return children;
+};
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className=" min-h-screen max-w-full overflow-x-hidden">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* it musst be a protected oute */}
+        <Route
+          path="/app"
+          element={
+            <ClerkProtected>
+              <AppShell />
+            </ClerkProtected>
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="create-invoice" element={<CreateInvoice/>}/>
+          <Route path="invoices" element={<Invoices/>}/>
+          {/* <Route path="/sign-in" element={<SignIn routing="path" path="/sign-in" />} /> */}
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
